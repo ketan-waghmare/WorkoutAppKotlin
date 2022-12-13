4d1664a2a4c206e4200de696d7404b64530851a1
ketan <ketan,waghmare@indexnine,com>
2022-12-13 16:09:20 +0530

    - viewbinding added

--- app/src/main/java/com/game/workoutappkotlin/MainActivity.kt
+++ app/src/main/java/com/game/workoutappkotlin/MainActivity.kt
@@ -2,10 +2,28 @@ package com.game.workoutappkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.game.workoutappkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding : ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.flStart?.setOnClickListener{
+            Toast.makeText(this,
+                    "Here will start the exercise",
+            Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
